405bbd9005670841b1bba65dcf36e0df35c0aec0
user-azizul <azizul.contact.me@gmail.com>
2025-05-09 15:22:25 +0300

    [nav ui] completed

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0]">
+        <h1 className="text-center text-3xl mt-5"> Chai aur code</h1>
+      </main>
+      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0]">
+        <h1 className="text-center text-3xl mt-5"> Chai aur code</h1>
+      </main>
+    </>
+  );
+}
